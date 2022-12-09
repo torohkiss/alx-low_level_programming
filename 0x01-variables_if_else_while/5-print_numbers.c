3f28e2b50e9130f0a13c062e028e5cf6ce8f5c20
torohkiss <torohkiss@yahoo.com>
2022-12-09 15:30:02 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
