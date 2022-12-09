8d4b0f2561fd35a0db75c72a4d072e8d830d4ead
torohkiss <torohkiss@yahoo.com>
2022-12-09 15:34:59 -0800

    task 7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while(i < 10)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
