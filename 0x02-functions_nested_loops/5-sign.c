a9b38855bd50e910550096de1d55fb3616662bf8
torohkiss <torohkiss@yahoo.com>
2022-12-14 12:28:02 -0800

    sgn

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - prints sign
+ * @n: number to print sign for
+ *
+ * Return: Always 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
