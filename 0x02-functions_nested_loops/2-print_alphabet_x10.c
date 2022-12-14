0b9ed1bbf4ebaac3b6fc421ca1b050755d05421c
torohkiss <torohkiss@yahoo.com>
2022-12-14 07:16:07 -0800

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet x10
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+
+	a = 0;
+	while (a <= 10)
+	{
+		char s;
+
+		s = 'a';
+		while (s <= 'z')
+		{
+			putchar(s);
+			s++;
+		}
+		a++;
+	}
+	putchar('\n');
+}
