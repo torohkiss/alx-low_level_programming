bd20792f4e96165f2e9ec3f53fed90ffdf5f234f
torohkiss <torohkiss@yahoo.com>
2022-12-14 13:22:47 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - computes abstract value of a num
+ * @n: num to compute
+ *
+ * Return: 0
+ */
+
+int _abs(int)
+{
+	int a;
+
+	if (n >= 0)
+		a = n;
+	else
+		a = n * -1;
+	return (a);
+}
