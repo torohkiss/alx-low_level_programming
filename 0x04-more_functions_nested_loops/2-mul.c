0015481d453fcfa3f412b19a69298181ff34b794
torohkiss <torohkiss@yahoo.com>
2022-12-15 05:15:48 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - muktuplies integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
