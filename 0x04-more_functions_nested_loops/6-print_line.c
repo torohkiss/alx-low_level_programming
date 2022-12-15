4b386f0bb3214808aca6d86594240c39397233e0
torohkiss <torohkiss@yahoo.com>
2022-12-15 13:49:06 -0800

    used for loop

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,11 +12,10 @@ void print_line(int n)
 
 	if (n <= 0)
 		_putchar('\n');
-	a = 0;
-	while (a < n)
+	else
 	{
-		_putchar('-');
-		a++;
+		for (a = 0; a < n; a++)
+			_putchar('_');
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
