b48bc2b4305c03a7ef24f06afa0c19282f90c263
torohkiss <torohkiss@yahoo.com>
2022-12-15 05:50:31 -0800

    print nums

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	c = 0;
+	while (c <= 9)
+	{
+		_putchar(c + '0');
+		c++;
+	}
+	_putchar('\n');
+}
