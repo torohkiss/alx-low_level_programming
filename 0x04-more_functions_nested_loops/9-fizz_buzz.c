7db52f0b92678416c5c08e7c0d85d1dff25d2e6c
torohkiss <torohkiss@yahoo.com>
2022-12-15 14:53:41 -0800

    added new line

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,15 +14,16 @@ int main(void)
 	for (a = 1; a <= 100; a++)
 	{
 		if (a % 15 == 0)
-			printf("FizzBuzz");
-		else if (a % 5 == 0)
-			printf("Buzz");
+			printf("%s", "FizzBuzz");
 		else if (a % 3 == 0)
-			printf("Fizz");
+			printf("%s", "Fizz");
+		else if (a % 3 == 0)
+			printf("%s", "Buzz");
 		else
 			printf("%d", a);
 		if (a != 100)
 			printf(" ");
 	}
+	printf("\n");
 	return (0);
 }
