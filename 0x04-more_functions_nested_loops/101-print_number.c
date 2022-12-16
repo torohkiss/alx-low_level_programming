5fd99138cd3453c6986feeefa13071e8fb7f339d
torohkiss <torohkiss@yahoo.com>
2022-12-16 14:35:15 -0800

    num

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * print_number - prints numbers
+ * @n: numbers to print
+ */
+
+void print_number(int n)
+{
+	unsigned int x;
+
+	x = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		x = -n;
+	}
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
+}
