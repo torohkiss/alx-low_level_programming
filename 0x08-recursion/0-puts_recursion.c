81e0ea2d54b70e6817c81d2f7d8b7f95cdc208ae
torohkiss <torohkiss@yahoo.com>
2023-01-04 13:24:02 -0800

    task o

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string
+ * @s: character to print
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
