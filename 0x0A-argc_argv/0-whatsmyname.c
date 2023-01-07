bcfc6eb41cd08470a02a6513821bd81ff06e4e36
torohkiss <torohkiss@yahoo.com>
2023-01-07 14:55:18 -0800

    task o

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[0]);
+	}
+	
+	return (0);
+}
