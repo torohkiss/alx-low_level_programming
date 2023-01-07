cd8e6e8566fe8400c2e483da8c70423bec3de378
torohkiss <torohkiss@yahoo.com>
2023-01-07 15:05:03 -0800

    hmm

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - entry point
@@ -10,14 +11,9 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-
 	(void) argc;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[0]);
-	}
+	
+	printf("%s\n", argv[0]);
 	
 	return (0);
 }
