5219567e08a94b997849660b71d17e9ad66c354f
torohkiss <torohkiss@yahoo.com>
2023-01-08 05:15:34 -0800

    correction

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,19 +15,18 @@ int main(int argc, char *argv[])
 	int mul;
 
 	mul = 1;
-
-	if (argc < 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
+	if (argc == 3)
 	{
 		for (count = 1; count < argc; count++)
 		{
 			mul *= atoi(argv[count]);
 		}
 		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 	return (0);
 }
