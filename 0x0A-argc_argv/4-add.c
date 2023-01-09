3b9f28eb965b8fec534918ec081c1226d2f6bc77
torohkiss <torohkiss@yahoo.com>
2023-01-09 00:07:34 -0800

    correctiin

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,26 +13,27 @@ int main(int argc, char *argv[])
 {
 	int i;
 	int sum;
+	int j;
 
 	sum = 0;
 
-	if (argc > 1)
+	if (argc < 2)
 	{
-		for (i = 1; i < argc; i++)
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (*argv[i] != '1' && *argv[i] != '9')
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-
-		for (i = 1; i < argc; i++)
-			sum += atoi(argv[i]);
-		printf("%d\n", sum);
+		sum += atoi(argv[i]);
 	}
-	if (argc == 1)
-		printf("0\n");
-
+	printf("%d\n", sum);
 	return (0);
 }
