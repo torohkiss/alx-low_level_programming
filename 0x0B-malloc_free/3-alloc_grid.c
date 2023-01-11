6ad36f0db9e8716d38e178c734e7bba307b456f7
torohkiss <torohkiss@yahoo.com>
2023-01-11 08:50:18 -0800

    remived else

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,8 +17,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	else
-	{
+	
 		p = (int **) malloc(height * sizeof(int *));
 		if (!p)
 		{
@@ -44,5 +43,4 @@ int **alloc_grid(int width, int height)
 			}
 		}
 		return (p);
-	}
 }
