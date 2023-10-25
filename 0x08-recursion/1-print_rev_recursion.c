4daa18c075689ee99664380f0f03a9c1e22f46e5
torohkiss <torohkiss@yahoo.com>
2023-10-25 16:57:45 -0700

    task 1a

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,11 +7,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\0');
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
 }
