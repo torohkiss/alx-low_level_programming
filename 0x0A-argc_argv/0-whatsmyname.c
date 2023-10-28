95941618c54269501d80aff25a3a4cb848788f19
torohkiss <torohkiss@yahoo.com>
2023-10-27 21:20:13 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -9,10 +8,9 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-	int __attribute__ ((unused)) argc;
 
 	return (0);
 }
