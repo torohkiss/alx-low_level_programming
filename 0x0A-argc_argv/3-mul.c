242cdbf1a7206d8f315a0cba9c88e416891db649
torohkiss <torohkiss@yahoo.com>
2023-10-27 21:35:01 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - msin function
+ * @argc: argumebt ciubt
+ * @argv: arg vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
