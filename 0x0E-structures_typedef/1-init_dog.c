4fa13ad3d34593ed3a6d94fe6bc6559c7d0a4f5f
torohkiss <torohkiss@yahoo.com>
2023-11-06 15:04:00 -0800

    tasj 1b

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,10 +10,6 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog dp;
-	d = &dp;
-
-	d = malloc(sizeof(struct dog));
 	if (d == NULL)
 		return (NULL);
 	d->name = name;
