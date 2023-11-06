93948a0fa5d17ba08fa03e9b80c6245ba55faddd
torohkiss <torohkiss@yahoo.com>
2023-11-06 15:52:22 -0800

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_dog - frees dogs.
+ * @d: pointer to syruct
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == 0)
+		return;
+	
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
