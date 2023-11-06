444e69147978c136c64ce6cfebf79c589759d91f
torohkiss <torohkiss@yahoo.com>
2023-11-06 14:38:40 -0800

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - a struct named dog
+ * @name: the dogs name
+ * @age: the dogs sgae
+ * @owner: the digs ownwr
+ *
+ * Description: thats all o for dogs
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
