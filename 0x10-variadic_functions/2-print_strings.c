8c1b52a60af909e85d05e8e6fae39f3401d235e6
torohkiss <torohkiss@yahoo.com>
2023-11-09 15:36:29 -0800

    task 2a

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,6 +14,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *str;
 
+	va_start(ap, n);
+
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(ap, char *);
