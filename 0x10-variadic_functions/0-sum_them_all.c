17efa3edde7e0afc959ed2fd36195b378df00574
torohkiss <torohkiss@yahoo.com>
2023-11-09 13:58:38 -0800

    task 0b

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,8 +9,8 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int sum = 0, i;
 	va_list ap;
-	unsigned int i, sum = 0;
 
 	if (n == 0)
 		return (0);
@@ -19,8 +19,8 @@ int sum_them_all(const unsigned int n, ...)
 	
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
-
 	va_end(ap);
+	
 	return (sum);
 }
 
