7500645cbb88feb35d9a5f9d0e7b4e9480942fa2
torohkiss <torohkiss@yahoo.com>
2023-11-16 15:12:32 -0800

    task 1b

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,12 +7,12 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	if (h == NULL)
-		printf("[0] (nil)\n");
-
 	while (h != NULL)
 	{
-		printf("[%d] (%c)\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		count++;
 	}
