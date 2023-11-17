2c7ab8d14be7bd9b87254fd1adde6ed6fd1e659b
torohkiss <torohkiss@yahoo.com>
2023-11-16 16:09:52 -0800

    tadk 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees the kist
+ * @head: pointer to head
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	free(list_t);
+}
