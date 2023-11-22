364ab562ef9208d285777c0f5d0061fa346bf815
torohkiss <torohkiss@yahoo.com>
2023-11-22 13:50:09 -0800

    task 3d

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - adds a new node at the 
+ * add_nodeint_end - adds a new node at the
  * end of a listint_t list.
  * @head: the list
  * @n: the data
@@ -17,6 +17,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	temp = malloc(sizeof(listint_t));
 	if (!temp)
 		return (NULL);
+
 	temp->next = NULL;
 	temp->n = n;
 
@@ -25,9 +26,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	else
 	{
 		ptr = *head;
-		
+
 		while (ptr->next != NULL)
 			ptr = ptr->next;
+
 		ptr->next = temp;
 	}
 
