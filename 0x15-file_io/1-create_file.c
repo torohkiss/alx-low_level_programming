ae90f093b4e6e6c647f608e19d0fb38528a090d5
torohkiss <torohkiss@yahoo.com>
2023-11-27 17:17:59 -0800

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,12 +10,12 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file, i, wr;
+	int fp, i, wr;
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (file == -1)
+	fp = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fp == -1)
 		return (-1);
 
 	if (text_content)
@@ -27,6 +27,6 @@ int create_file(const char *filename, char *text_content)
 		if (wr != i)
 			return (-1);
 	}
-	close(file);
-	return (0);
+	close(fp);
+	return (1);
 }
